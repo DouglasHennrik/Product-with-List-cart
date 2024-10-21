8f2b5af3f1858c830ec4cc667f7108528d4d89d2
Douglas Hennrik <douglashennrik@gmail.com>
2024-10-21 10:28:38 -0300

    feat: Estrutura HTML

--- index.ts
+++ index.ts
@@ -5,10 +5,19 @@ class Product {
   private _name: string;
   private _category: string;
   private _imageUrl: string;
-  constructor(id: string, name: string, category: string, imageUrl: string) {
+  private _price: number;
+
+  constructor(
+    id: string,
+    name: string,
+    category: string,
+    imageUrl: string,
+    price: number
+  ) {
     this._id = id;
     this._name = name;
     this._category = category;
     this._imageUrl = imageUrl;
+    this._price = price;
   }
 }
